b0b0b3e90e215ac06b82cc2d3798801b06a95546
Jktran03 <jktran9@cougarnet.uh.edu>
2024-09-30 16:39:57 -0500

    Commit

--- Graded Practice Exercise 2/task1/task1.js
+++ Graded Practice Exercise 2/task1/task1.js
@@ -8,40 +8,38 @@
 
       Filename: task1_JS.JavaScript
  */
-alert("JavaScript is working!"); // This should pop up when the page loads
 function FahrenheitToCelsius(degree) {
-      return (degree - 32) / 1.8;
-  }
-  
-  // Function to convert Celsius to Fahrenheit
-  function CelsiusToFahrenheit(degree) {
-      return (degree * 1.8) + 32;
-  }
-  
-  // Event handler for Celsius input (convert when the user changes value)
-  document.getElementById("cValue").oninput = function() {
-      let cDegree = document.getElementById("cValue").value;
-      
-      if (cDegree !== '') {
-          // Convert Celsius to Fahrenheit and update the Fahrenheit input
-          let fValue = CelsiusToFahrenheit(parseFloat(cDegree)).toFixed(2);
-          document.getElementById("fValue").value = fValue;
-      } else {
-          // Clear the Fahrenheit input if Celsius input is empty
-          document.getElementById("fValue").value = '';
-      }
-  };
-  
-  // Event handler for Fahrenheit input (convert when the user changes value)
-  document.getElementById("fValue").oninput = function() {
-      let fDegree = document.getElementById("fValue").value;
-      
-      if (fDegree !== '') {
-          // Convert Fahrenheit to Celsius and update the Celsius input
-          let cValue = FahrenheitToCelsius(parseFloat(fDegree)).toFixed(2);
-          document.getElementById("cValue").value = cValue;
-      } else {
-          // Clear the Celsius input if Fahrenheit input is empty
-          document.getElementById("cValue").value = '';
-      }
-  };
+    return (degree - 32) / 1.8;
+}
+
+function CelsiusToFahrenheit(degree) {
+    return (degree * 1.8) + 32;
+}
+
+document.getElementById("cValue").oninput = convertCelsiusToFahrenheit;
+document.getElementById("cValue").onblur = convertCelsiusToFahrenheit;
+
+document.getElementById("fValue").oninput = convertFahrenheitToCelsius;
+document.getElementById("fValue").onblur = convertFahrenheitToCelsius;
+
+function convertCelsiusToFahrenheit() {
+    let cDegree = document.getElementById("cValue").value;
+
+    if (cDegree !== '') {
+        let fValue = CelsiusToFahrenheit(parseFloat(cDegree)).toFixed(2);
+        document.getElementById("fValue").value = fValue;
+    } else {
+        document.getElementById("fValue").value = '';
+    }
+}
+
+function convertFahrenheitToCelsius() {
+    let fDegree = document.getElementById("fValue").value;
+
+    if (fDegree !== '') {
+        let cValue = FahrenheitToCelsius(parseFloat(fDegree)).toFixed(2);
+        document.getElementById("cValue").value = cValue;
+    } else {
+        document.getElementById("cValue").value = '';
+    }
+}
